4a0d553858fb088ac71c4d8c1db07912b3b73fe7
Pedrovicentte <pedrohenei092@gmail.com>
2025-10-04 20:59:32 -0300

    teste

---
+++ Veiculo.py
@@ -0,0 +1,6 @@
+class veiculo:
+    def __init__(self, placa, valor, alugado, histórico):
+        self.__placa = placa
+        self.__valor = valor
+        self.__alugado = alugado
+        self.__histórico = histórico
